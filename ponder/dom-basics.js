0c7d6113c6c7dd9187a3afa99225b85b928ea15b
Helaman Tillotson <helaman.tillotson@gmail.com>
2025-01-16 09:57:11 -0700

    finished setting up dark mode for the mission project

--- ponder/dom-basics.js
+++ ponder/dom-basics.js
@@ -1,14 +1,14 @@
 const newParagraph = document.createElement("p");
-newParagraph.innerText = "Added with Javascript!";
-document.body.appendChild(newParagraph);
+newParagraph.textContent = "Added with Javascript!";
+document.body.append(newParagraph);
 const newImage = document.createElement('img');
-newImage.setAttribute('src', 'https://picsum.photos/200');
-newImage.setAttribute('alt', 'Random image from picsum.');
-document.body.appendChild(newImage);
+newImage.src = "https://picsum.photos/200/300";
+newImage.alt = "Random image from picsum.";
+document.querySelector("section").prepend(newImage);
 const newDiv = document.createElement("div");
 newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
-document.body.appendChild(newDiv);
+document.body.append(newDiv);
 
 const newSection = document.createElement('section');
 newSection.innerHTML = '<h2>DOM Basics</h2><p>This was added through Javascript</p>';
-document.body.appendChild(newSection);
+document.body.append(newSection);
