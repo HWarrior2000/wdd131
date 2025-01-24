8bee60a26bed9a7482f58e50777ad79fe9d871d4
Helaman Tillotson <helaman.tillotson@gmail.com>
2025-01-23 17:45:56 -0700

    teset

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -34,13 +34,17 @@ function viewerTemplate(pic, alt){
 
 function viewHandler(event) {
 	// create a variable to hold the element that was clicked on from event.target
-    const image = document.event.target;
+    const image = event.target;
+    console.log(image);
 	// get the src attribute from that element and 'split' it on the "-"
-
+    const source = image.src.split("-");
+    console.log(source);
 	// construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
-
+    const newImage = source[0] + "-full.jpg";
+    console.log(newImage);
 	// insert the viewerTemplate into the top of the body element
 	// (element.insertAdjacentHTML("afterbegin", htmltoinsert))
+    document.querySelector("body").insertAdjacentHTML("afterbegin", viewerTemplate(newImage, "hello"));
 
 	// add a listener to the close button (X) that calls a function called closeViewer when clicked
 
@@ -49,3 +53,4 @@ function viewHandler(event) {
 handleResize();
 document.querySelector("#menuButton").addEventListener("click", toggleMenu);
 window.addEventListener("resize", handleResize);
+document.addEventListener("click", viewHandler);
